7297a34869ba0e9a71867dde6217ba5631e98a24
Noer Paanakker <noer@healthyworkers.nl>
2017-09-30 19:49:45 +0200

    created first reducer for all posts state object. used lodash mapKeys

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/App';
-import Bye from './components/Bye';
+import PostsIndex from './components/PostsIndex';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
 	  <BrowserRouter>
 	  	<div>
-	  		<Route path='/hello' component={App}/>
-	  		<Route path='/bye' component={Bye}/>
+	  		<Route path='/' component={PostsIndex}/>
+	  		<Route path='/hi' component={App}/>
 	  	</div>
 	  </BrowserRouter>
     
